906ddccd6ee12d188aa4836c9a7dae0711fa0525
Fuad Seffa <fuadjemal@gmail.com>
2017-07-13 00:38:31 +0300

    DNA sub-match

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,56 +1,41 @@
 #include <iostream>
+#include <string>
+#include <stdio.h>
 #include "console.h"
-#include "gwindow.h" // for GWindow
-#include "simpio.h"  // for getLine
-#include "vector.h"  // for Vector
 using namespace std;
 
+int findDNAMatch(string s1, string s2, int start = 0);
+
 int main() {
-    cout << "This sample project helps you check and make sure that" << endl;
-    cout << "you have successfully install Qt Creator, C++, and Java," << endl;
-    cout << "and that the Stanford C++ libraries work on your machine." << endl;
-    cout << endl;
-    cout << "If this project is working properly on your computer," << endl;
-    cout << "You should see a \"Sample Project\" window with a" << endl;
-    cout << "smiley face pop up when you build and run it." << endl;
-    cout << endl;
-    cout << "If it doesn't run, check that you have installed" << endl;
-    cout << "Java JDK properly, as well as C++ and Qt Creator." << endl;
-    cout << endl;
+    string hay = "TAACGGTACGTC";
+    string sub_1 = "TTGCC";
+    string sub_2 = "TGC";
+    const char *fmt = "Location of sub(%s) over (%s) is [%d]\n";
 
-    GWindow window(300, 200);
-    window.setColor("White");
-    window.clear();
-    window.setWindowTitle("Sample Project");
-    window.setColor("Black");
-    window.drawOval( 10, 10, 180, 180);   // head
-    window.fillOval( 50, 50,  20,  20);   // eye
-    window.fillOval(130, 50,  20,  20);   // eye
-    window.fillOval( 95, 95,  10,  10);   // nose
-
-    // draw the mouth using a Vector to make sure the Stanford collections work
-    Vector<GPoint> mouth;
-    mouth.add(GPoint( 40, 120));
-    mouth.add(GPoint( 70, 150));
-    mouth.add(GPoint(130, 150));
-    mouth.add(GPoint(160, 120));
-    for (int i = 0; i < mouth.size() - 1; i++) {
-        GPoint p1 = mouth.get(i);
-        GPoint p2 = mouth.get(i + 1);
-        window.drawLine(p1, p2);   // mouth
-    }
-
-    window.setVisible(true);
-
-    window.requestFocus();
-    pause(3000);
-
-    getLine("Press [Enter] to close the window... ");
-    window.requestFocus();
-    window.close();
-    exitGraphics();
-
-    cout << endl << "Program completed." << endl;
+    printf("\n\n");
+    printf(fmt, sub_1.c_str(), hay.c_str(), findDNAMatch(hay, sub_1));
+    printf(fmt, sub_2.c_str(), hay.c_str(), findDNAMatch(hay, sub_2));
+    printf(fmt, sub_2.c_str(), hay.c_str(), findDNAMatch(hay, sub_2, findDNAMatch(hay, sub_2) + 1));
+    printf("\n\n");
 
     return 0;
+}
+
+int findDNAMatch(string hey, string needle, int start) {
+    string mirror_dna;
+    for (int i = 0; i < needle.length(); i++) {
+        char ch = needle[i];
+        char inverse;
+
+        switch (ch) {
+            case 'T': inverse = 'A'; break;
+            case 'A': inverse = 'T'; break;
+            case 'C': inverse = 'G'; break;
+            case 'G': inverse = 'C'; break;
+        }
+
+        mirror_dna += inverse;
+    }
+
+    return hey.find(mirror_dna, start);
 }
