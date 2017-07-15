0578d5c9773bf61084fd0e7c129f22ee874d0df3
Fuad Seffa <fuadjemal@gmail.com>
2017-07-15 10:48:49 +0300

    Implement 2 types of scramble logic

--- src/app.cpp
+++ src/app.cpp
@@ -9,15 +9,18 @@ using namespace std;
 
 string promptUserForFile(ifstream & infile, string prompt = "");
 
-string scrambleLine(string & line);
+string scrambleLine1(string & line);
+string scrambleLine2(string & line);
 
 int main() {
     ifstream infile;
     promptUserForFile(infile, "Input file: ");
 
+    //setRandomSeed(time(NULL));
+    setRandomSeed(0);
     string line;
     while (getline(infile, line)) {
-        cout << scrambleLine(line) << endl;
+        cout << scrambleLine2(line) << endl;
     }
     infile.close();
     return 0;
@@ -38,17 +41,18 @@ string promptUserForFile(ifstream & infile, string prompt) {
     }
 }
 
-string scrambleLine(string &line) {
-    string UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    string LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
-    int ALPHABET_SIZE = UPPER_CASE.size();
+string UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+int ALPHABET_SIZE = UPPER_CASE.size();
+
+string scrambleLine1(string &line) {
     ostringstream result;
     istringstream lineStream(line);
 
     int ch;
     while ((ch = lineStream.get()) != EOF)  {
         if (!isalpha(ch)) {
-            result << ch;
+            result << (char)ch;
             continue;
         }
 
@@ -65,3 +69,23 @@ string scrambleLine(string &line) {
     return result.str();
 }
 
+string scrambleLine2(string &line) {
+    string result;
+    for (int i = 0; i < line.size(); i++) {
+        char ch = line[i];
+        if (!isalpha(ch)) {
+            result += ch;
+            continue;
+        }
+
+        int rand_index = randomInteger(0, ALPHABET_SIZE);
+        char replacement;
+        if (isupper(ch)) {
+            replacement = UPPER_CASE[rand_index];
+        } else {
+            replacement = LOWER_CASE[rand_index];
+        }
+        result += replacement;
+    }
+    return result;
+}
