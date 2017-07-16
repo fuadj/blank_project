da653974ae67903b8f9eb88768537e53494b3248
Fuad Seffa <fuadjemal@gmail.com>
2017-07-16 21:49:05 +0300

    Airport Codes

--- src/app.cpp
+++ src/app.cpp
@@ -1,80 +1,44 @@
 #include "console.h" // "console.h" needs to be included to work with StanfordLib, it otherwise WON'T compile
 #include <iostream>
-#include <iomanip>
-#include "queue.h"
-#include "random.h"
+#include <fstream>
+#include <string>
+#include "error.h"
+#include "map.h"
+#include "strlib.h"
+
 using namespace std;
 
-const double ARRIVAL_PROBABILITY = 0.05;
-//const double ARRIVAL_PROBABILITY = 0.1;
-const int MIN_SERVICE_TIME = 5;
-const int MAX_SERVICE_TIME = 15;
-const int SIMULATION_TIME = 2000;
-
-void runSimulation(int & nServed, int & totalWait, int & totalLength);
-void printReport(int nServed, int totalWait, int totalLength);
+void readCodeFile(string filename, Map<string, string> & map);
 
 int main() {
-    int nServed;
-    int totalWait;
-    int totalLength;
-    runSimulation(nServed, totalWait, totalLength);
-    printReport(nServed, totalWait, totalLength);
+    Map<string, string> airportCodes;
+    readCodeFile("AirportCodes.txt", airportCodes);
+    while (true) {
+        string line;
+        cout << "Airport code: ";
+        getline(cin, line);
+        if (line == "") break;
+        string code = toUpperCase(line);
+        if (airportCodes.containsKey(code)) {
+            cout << code << " is in " << airportCodes.get(code) << endl;
+        } else {
+            cout << "There is no such airport code" << endl;
+        }
+    }
     return 0;
 }
 
-void runSimulation(int & nServed, int & totalWait, int & totalLength) {
-    Queue<int> queue;
-    int timeRemaining = 0;
-    nServed = 0;
-    totalWait = 0;
-    totalLength = 0;
-    for (int t = 0; t < SIMULATION_TIME; t++) {
-        if (randomChance(ARRIVAL_PROBABILITY)) {
-            queue.enqueue(t);
+void readCodeFile(string filename, Map<string, string> &map) {
+    ifstream infile;
+    infile.open(filename.c_str());
+    if (infile.fail()) error("Can't read the data file");
+    string line;
+    while (getline(infile, line)) {
+        if (line.length() < 4 || line[3] != '=') {
+            error("Illegal data line: " + line);
         }
-        if (timeRemaining > 0) {
-            timeRemaining--;
-        } else if (!queue.isEmpty()) {
-            totalWait += t - queue.dequeue();
-            nServed++;
-            timeRemaining = randomInteger(MIN_SERVICE_TIME, MAX_SERVICE_TIME);
-        }
-        totalLength += queue.size();
+        string code = toUpperCase(line.substr(0, 3));
+        map.put(code, line.substr(4));
     }
+    infile.close();
 }
-
-void printReport(int nServed, int totalWait, int totalLength) {
-    cout << "Simulation results given the following constants:" << endl;
-    cout << fixed << setprecision(2);
-    cout << "  SIMULATION_TIME:     " << setw(4)
-         << SIMULATION_TIME << endl;
-    cout << "  ARRIVAL_PROBABILITY: " << setw(7)
-         << ARRIVAL_PROBABILITY << endl;
-    cout << "  MIN_SERVICE_TIME:    " << setw(4)
-         << MIN_SERVICE_TIME << endl;
-    cout << "  MAX_SERVICE_TIME:    " << setw(4)
-         << MAX_SERVICE_TIME << endl;
-    cout << endl;
-    cout << "Customers served:      " << setw(4) << nServed << endl;
-    cout << "Average waiting time:  " << setw(7)
-         << double(totalWait) / nServed << endl;
-    cout << "Average queue length:  " << setw(7)
-         << double(totalLength) / SIMULATION_TIME << endl;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
