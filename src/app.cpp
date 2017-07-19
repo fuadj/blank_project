ee0debe371ddd7551591d538671c16a1be25cba1
Fuad Seffa <fuadjemal@gmail.com>
2017-07-19 18:36:20 +0300

    Word Frequency Counter

--- src/app.cpp
+++ src/app.cpp
@@ -1,19 +1,60 @@
 #include "console.h" // "console.h" needs to be included to work with StanfordLib, it otherwise WON'T compile
 #include <iostream>
-#include "lexicon.h"
-
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <cctype>
+#include "filelib.h"
+#include "map.h"
+#include "strlib.h"
+#include "vector.h"
 using namespace std;
+
+void countWords(istream & stream, Map<string, int> & wordCounts);
+void displayWordCounts(Map<string, int> & wordCounts);
+void extractWords(string line, Vector<string> & words);
+
 int main() {
-    Lexicon english("EnglishWords.dat");
-    string word = "xx";
-    for (char c0 = 'a'; c0 <= 'z'; c0++) {
-        word[0] = c0;
-        for (char c1 = 'a'; c1 <= 'z'; c1++) {
-            word[1] = c1;
-            if (english.contains(word)) {
-                cout << word << endl;
+    ifstream infile;
+    Map<string, int> wordCounts;
+    promptUserForFile(infile, "Input file: ");
+    countWords(infile, wordCounts);
+    infile.close();
+    displayWordCounts(wordCounts);
+    return 0;
+}
+
+void countWords(istream &stream, Map<string, int> &wordCounts) {
+    Vector<string> lines, words;
+    readEntireFile(stream, lines);
+    for (string line : lines) {
+        extractWords(line, words);
+        for (string word : words) {
+            wordCounts[toLowerCase(word)]++;
+        }
+    }
+}
+
+void displayWordCounts(Map<string, int> &wordCounts) {
+    for (string word : wordCounts) {
+        cout << left << setw(15) << word
+             << right << setw(5) << wordCounts[word] << endl;
+    }
+}
+
+void extractWords(string line, Vector<string> &words) {
+    words.clear();
+    int start = -1;
+    for (int i = 0; i < line.length(); i++) {
+        if (isalpha(line[i])) {
+            if (start == -1) start = i;
+        } else {
+            if (start >= 0) {
+                words.add(line.substr(start, i - start));
+                start = -1;
             }
         }
     }
-    return 0;
+
+    if (start >= 0) words.add(line.substr(start));
 }
