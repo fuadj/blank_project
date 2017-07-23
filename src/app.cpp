f8f73dc4c1bb79f024089866f26e06eb21050083
Fuad Seffa <fuadjemal@gmail.com>
2017-07-23 18:10:16 +0300

    Cannon ball counter
    It looks simple, but the question is asking for total # of
    balls in the entire pyramid. The easier question is for # of
    balls in the last level.

--- src/app.cpp
+++ src/app.cpp
@@ -3,6 +3,33 @@
 
 using namespace std;
 
+int cannonBall(int height);
+
 int main() {
+    for (int i = 1; i <= 4; i++) {
+        cout << "Cannon Height " << i << " total balls " << cannonBall(i) << endl;
+    }
     return 0;
+}
+
+int cannonBallLevels(int height, int & ballCount) {
+    if (height <= 1)  {
+        ballCount++;
+        return 1;
+    }
+
+    int upperLevel = cannonBallLevels(height - 1, ballCount);
+
+    int currentLevel = upperLevel + (2 * height - 1);
+    ballCount += currentLevel;
+
+    return currentLevel;
+}
+
+int cannonBall(int height) {
+    int ballCount = 0;
+
+    cannonBallLevels(height, ballCount);
+
+    return ballCount;
 }
