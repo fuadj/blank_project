bec1db375467b5680a1b89f8dcdf917846e7a954
Fuad Seffa <fuadjemal@gmail.com>
2017-07-24 16:50:01 +0300

    Recursive mondrian art

--- src/app.cpp
+++ src/app.cpp
@@ -1,57 +1,63 @@
 #include <iostream>
 #include "console.h" // This NEEDS to be included, it WON'T compile otherwise
-#include <string>
-#include "set.h"
-#include "simpio.h"
-
+#include "gwindow.h"
+#include "random.h"
 using namespace std;
 
-Set<string> generatePermutations(const string & s);
-Set<string> generatePermutationsBook(string s);
+const double MIN_AREA = 10000;
+const double MIN_EDGE = 10;			/* Smallest edge length allowed */
+
+void subDivideCanvas(GWindow & gw, double x, double y,
+                     double width, double height);
 
 int main() {
-    string str = getLine("Enter a string: ");
-    cout << "The permutations of \"" << str << "\" are:" << endl;
-    for (string s : generatePermutationsBook(str)) {
-        cout << "   \"" << s << "\"" << endl;
-    }
+    const int WIDTH = 800;
+    const int HEIGHT = 700;
+    const int EDGE = 50;
+    GWindow gw(WIDTH, HEIGHT);
+
+    subDivideCanvas(gw, EDGE, EDGE, WIDTH - 2 * EDGE, HEIGHT - 2 * EDGE);
 
     return 0;
 }
 
-void generatePermutationsRecursive(const string & sofar, const Set<char> & chars, Set<string> & permutations) {
-    if (chars.isEmpty()) {
-        permutations.add(sofar);
-    } else {
-        for (char ch : chars) {
-            generatePermutationsRecursive(sofar + ch, chars - ch, permutations);
+void subDivideCanvas(GWindow & gw, double x, double y, double width, double height) {
+    if (width * height >= MIN_AREA) {
+        int bm_r = 0xff & randomInteger(128, 255);
+        int bm_g = 0xff & randomInteger(28, 140);
+        int bm_b = 0xff & randomInteger(28, 85);
+        int bm_color = 0xffffffff & (bm_r << 16 | bm_g << 8 | bm_b);
+
+        int am_r = randomInteger(0, 28);
+        int am_g = randomInteger(10, 128);
+        int am_b = randomInteger(70, 200);
+        int am_color = 0xffffffff & (am_r << 16 | am_g << 8 | am_b);
+
+        if (width > height) {
+            double mid = randomReal(MIN_EDGE, width - MIN_EDGE);
+
+            gw.setColor(bm_color);
+            gw.fillRect(GRectangle(x, y, mid, height));
+            gw.setColor(am_color);
+            gw.fillRect(GRectangle(x + mid, y, width - mid, height));
+
+            //gw.drawLine(x + mid, y, x + mid, y + height);
+
+            subDivideCanvas(gw, x, y, mid, height);
+            subDivideCanvas(gw, x + mid, y, width - mid, height);
+        } else {
+            double mid = randomReal(MIN_EDGE, height - MIN_EDGE);
+
+            gw.setColor(bm_color);
+            gw.fillRect(GRectangle(x, y, width, mid));
+            gw.setColor(am_color);
+            gw.fillRect(GRectangle(x, y + mid, width, height - mid));
+
+            //gw.drawLine(x, y + mid, x + width, y + mid);
+
+            subDivideCanvas(gw, x, y, width, mid);
+            subDivideCanvas(gw, x, y + mid, width, height - mid);
         }
     }
 }
 
-Set<string> generatePermutations(const string & s) {
-    Set<char> chars;
-    Set<string> permutations;
-    for (char ch : s) {
-        chars.add(ch);
-    }
-    generatePermutationsRecursive("", chars, permutations);
-    return permutations;
-}
-
-Set<string> generatePermutationsBook(string str) {
-    Set<string> result;
-    if (str == "") {
-        result += "";
-    } else {
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str[i];
-            string rest = str.substr(0, i) + str.substr(i+1);
-            for (string s : generatePermutationsBook(rest)) {
-                result += ch + s;
-            }
-        }
-    }
-
-    return result;
-}
