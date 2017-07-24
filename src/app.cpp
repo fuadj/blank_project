7f7b22666dd4187a1e8899499331542963d49676
Fuad Seffa <fuadjemal@gmail.com>
2017-07-24 17:29:15 +0300

    Mandelbort snowflake

--- src/app.cpp
+++ src/app.cpp
@@ -1,63 +1,74 @@
 #include <iostream>
 #include "console.h" // This NEEDS to be included, it WON'T compile otherwise
+#include <cmath>
+#include <unistd.h>
 #include "gwindow.h"
-#include "random.h"
+#include "gtypes.h"
 using namespace std;
 
-const double MIN_AREA = 10000;
-const double MIN_EDGE = 10;			/* Smallest edge length allowed */
+const double SIZE = 200;
+//const int ORDER = 4;
+const int ORDER = 5;
+const int SLEEP_U_S = 1000;
 
-void subDivideCanvas(GWindow & gw, double x, double y,
-                     double width, double height);
+GPoint drawFractalLine(GWindow & gw, GPoint pt,
+                       double r, double theta, int order);
 
 int main() {
-    const int WIDTH = 800;
-    const int HEIGHT = 700;
-    const int EDGE = 50;
-    GWindow gw(WIDTH, HEIGHT);
+    GWindow gw;
+    double cx = gw.getWidth() / 2;
+    double cy = gw.getHeight() / 2;
 
-    subDivideCanvas(gw, EDGE, EDGE, WIDTH - 2 * EDGE, HEIGHT - 2 * EDGE);
+    GPoint pt(cx - SIZE / 2, cy - sqrt(3.0) * SIZE / 6);
+    /*
+    pt = drawFractalLine(gw, pt, SIZE, 0, ORDER);
+    pt = drawFractalLine(gw, pt, SIZE, -120, ORDER);
+    pt = drawFractalLine(gw, pt, SIZE, 120, ORDER);
+    */
+    pt = drawFractalLine(gw, pt, SIZE, 0, ORDER);
+    pt = drawFractalLine(gw, pt, SIZE, -90, ORDER);
+    pt = drawFractalLine(gw, pt, SIZE, -180, ORDER);
+    pt = drawFractalLine(gw, pt, SIZE, 90, ORDER);
 
     return 0;
 }
 
-void subDivideCanvas(GWindow & gw, double x, double y, double width, double height) {
-    if (width * height >= MIN_AREA) {
-        int bm_r = 0xff & randomInteger(128, 255);
-        int bm_g = 0xff & randomInteger(28, 140);
-        int bm_b = 0xff & randomInteger(28, 85);
-        int bm_color = 0xffffffff & (bm_r << 16 | bm_g << 8 | bm_b);
-
-        int am_r = randomInteger(0, 28);
-        int am_g = randomInteger(10, 128);
-        int am_b = randomInteger(70, 200);
-        int am_color = 0xffffffff & (am_r << 16 | am_g << 8 | am_b);
-
-        if (width > height) {
-            double mid = randomReal(MIN_EDGE, width - MIN_EDGE);
-
-            gw.setColor(bm_color);
-            gw.fillRect(GRectangle(x, y, mid, height));
-            gw.setColor(am_color);
-            gw.fillRect(GRectangle(x + mid, y, width - mid, height));
-
-            //gw.drawLine(x + mid, y, x + mid, y + height);
-
-            subDivideCanvas(gw, x, y, mid, height);
-            subDivideCanvas(gw, x + mid, y, width - mid, height);
-        } else {
-            double mid = randomReal(MIN_EDGE, height - MIN_EDGE);
-
-            gw.setColor(bm_color);
-            gw.fillRect(GRectangle(x, y, width, mid));
-            gw.setColor(am_color);
-            gw.fillRect(GRectangle(x, y + mid, width, height - mid));
-
-            //gw.drawLine(x, y + mid, x + width, y + mid);
-
-            subDivideCanvas(gw, x, y, width, mid);
-            subDivideCanvas(gw, x, y + mid, width, height - mid);
-        }
+GPoint drawFractalLine(GWindow & gw, GPoint pt, double r, double theta, int order) {
+    if (order <= 0) {
+        usleep(SLEEP_U_S);
+        return gw.drawPolarLine(pt, r, theta);
+    } else {
+        GPoint save = pt;
+        pt = drawFractalLine(gw, pt, r / 3, theta, order - 1);
+        pt = drawFractalLine(gw, pt, r / 3, theta - 60, order - 1);
+        pt = drawFractalLine(gw, pt, r / 3, theta + 60, order - 1);
+        pt = drawFractalLine(gw, pt, r / 3, theta, order - 1);
+        /*
+        save = gw.drawPolarLine(save, r / 3, theta);
+        save = gw.drawPolarLine(save, r / 3, theta - 60);
+        save = gw.drawPolarLine(save, r / 3, theta + 60);
+        save = gw.drawPolarLine(save, r / 3, theta);
+        */
+        /*
+        pt = drawFractalLine(gw, pt, r / 4, theta, order - 1);
+        pt = drawFractalLine(gw, pt, r / 4, theta + 60, order - 1);
+        pt = drawFractalLine(gw, pt, r / 4, theta, order - 1);
+        pt = drawFractalLine(gw, pt, r / 4, theta - 60, order - 1);
+        pt = drawFractalLine(gw, pt, r / 4, theta, order - 1);
+        */
+        /*
+        pt = drawFractalLine(gw, pt, r / 3, theta + 60, order - 1);
+        pt = drawFractalLine(gw, pt, r / 3, theta - 60, order - 1);
+        pt = drawFractalLine(gw, pt, r / 3, theta, order - 1);
+        pt = drawFractalLine(gw, pt, r / 3, theta + 60, order - 1);
+        pt = drawFractalLine(gw, pt, r / 3, theta - 60, order - 1);
+        */
+        /*
+        save = gw.drawPolarLine(save, r / 3, theta);
+        save = gw.drawPolarLine(save, r / 3, theta + 60);
+        save = gw.drawPolarLine(save, r / 3, theta - 60);
+        save = gw.drawPolarLine(save, r / 3, theta);
+        */
+        return pt;
     }
 }
-
