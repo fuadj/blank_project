3ac434ed600990b49daea3c2d8dfd73de546eb9c
Fuad Seffa <fuadjemal@gmail.com>
2017-07-24 17:54:18 +0300

    Towers of hanoi with Stack

--- src/app.cpp
+++ src/app.cpp
@@ -1,74 +1,39 @@
 #include <iostream>
 #include "console.h" // This NEEDS to be included, it WON'T compile otherwise
-#include <cmath>
-#include <unistd.h>
-#include "gwindow.h"
-#include "gtypes.h"
+#include "stack.h"
+
 using namespace std;
 
-const double SIZE = 200;
-//const int ORDER = 4;
-const int ORDER = 5;
-const int SLEEP_U_S = 1000;
+struct Move {
+    int numPlates;
+    char source, dest, temp;
 
-GPoint drawFractalLine(GWindow & gw, GPoint pt,
-                       double r, double theta, int order);
+    Move(){}
+    Move(int n, char s, char d, char t): numPlates(n), source(s), dest(d), temp(t) {}
+};
+
+void moveTowersOfHanoiWithStack(int n, char source, char dest, char temp);
 
 int main() {
-    GWindow gw;
-    double cx = gw.getWidth() / 2;
-    double cy = gw.getHeight() / 2;
-
-    GPoint pt(cx - SIZE / 2, cy - sqrt(3.0) * SIZE / 6);
-    /*
-    pt = drawFractalLine(gw, pt, SIZE, 0, ORDER);
-    pt = drawFractalLine(gw, pt, SIZE, -120, ORDER);
-    pt = drawFractalLine(gw, pt, SIZE, 120, ORDER);
-    */
-    pt = drawFractalLine(gw, pt, SIZE, 0, ORDER);
-    pt = drawFractalLine(gw, pt, SIZE, -90, ORDER);
-    pt = drawFractalLine(gw, pt, SIZE, -180, ORDER);
-    pt = drawFractalLine(gw, pt, SIZE, 90, ORDER);
-
+    moveTowersOfHanoiWithStack(3, 'A', 'B', 'C');
     return 0;
 }
 
-GPoint drawFractalLine(GWindow & gw, GPoint pt, double r, double theta, int order) {
-    if (order <= 0) {
-        usleep(SLEEP_U_S);
-        return gw.drawPolarLine(pt, r, theta);
-    } else {
-        GPoint save = pt;
-        pt = drawFractalLine(gw, pt, r / 3, theta, order - 1);
-        pt = drawFractalLine(gw, pt, r / 3, theta - 60, order - 1);
-        pt = drawFractalLine(gw, pt, r / 3, theta + 60, order - 1);
-        pt = drawFractalLine(gw, pt, r / 3, theta, order - 1);
-        /*
-        save = gw.drawPolarLine(save, r / 3, theta);
-        save = gw.drawPolarLine(save, r / 3, theta - 60);
-        save = gw.drawPolarLine(save, r / 3, theta + 60);
-        save = gw.drawPolarLine(save, r / 3, theta);
-        */
-        /*
-        pt = drawFractalLine(gw, pt, r / 4, theta, order - 1);
-        pt = drawFractalLine(gw, pt, r / 4, theta + 60, order - 1);
-        pt = drawFractalLine(gw, pt, r / 4, theta, order - 1);
-        pt = drawFractalLine(gw, pt, r / 4, theta - 60, order - 1);
-        pt = drawFractalLine(gw, pt, r / 4, theta, order - 1);
-        */
-        /*
-        pt = drawFractalLine(gw, pt, r / 3, theta + 60, order - 1);
-        pt = drawFractalLine(gw, pt, r / 3, theta - 60, order - 1);
-        pt = drawFractalLine(gw, pt, r / 3, theta, order - 1);
-        pt = drawFractalLine(gw, pt, r / 3, theta + 60, order - 1);
-        pt = drawFractalLine(gw, pt, r / 3, theta - 60, order - 1);
-        */
-        /*
-        save = gw.drawPolarLine(save, r / 3, theta);
-        save = gw.drawPolarLine(save, r / 3, theta + 60);
-        save = gw.drawPolarLine(save, r / 3, theta - 60);
-        save = gw.drawPolarLine(save, r / 3, theta);
-        */
-        return pt;
+void moveTowersOfHanoiWithStack(int n, char source, char dest, char temp) {
+    Stack<Move> moves;
+
+    moves.push(Move(n, source, dest, temp));
+    while (!moves.isEmpty()) {
+        Move move = moves.pop();
+        if (move.numPlates == 1)
+            cout << move.source << " => " << move.dest << endl;
+        else if (move.numPlates > 1) {
+            moves.push(Move(move.numPlates - 1,
+                            move.temp, move.dest, move.source));
+            moves.push(Move(1,
+                            move.source, move.dest, move.temp));	// move the last plate
+            moves.push(Move(move.numPlates - 1,
+                            move.source, move.temp, move.dest));
+        }
     }
 }
