080a772a103841814f8f90ac048006915d7e5c67
Fuad Seffa <fuadjemal@gmail.com>
2017-07-25 18:03:56 +0300

    Measuring weight combinations

--- src/app.cpp
+++ src/app.cpp
@@ -2,32 +2,43 @@
 #include <string>
 #include "console.h"
 #include "vector.h"
-#include "lexicon.h"
 #include "simpio.h"
 
 using namespace std;
 
-void embeddedWords(string sofar, string letters, const Lexicon & lexicon);
+bool isMeasurable(int target, const Vector<int> & weights, Vector<int> correctWay);
+
+void printMeasurable(int target, const Vector<int> & weights) {
+    cout << "Measuring " << target << ":" << endl;
+    isMeasurable(target, weights, Vector<int>());
+    //cout << target << " is" << ( ? " " : " not ") << "measurable" << endl;
+}
 
 int main() {
-    Lexicon lexicon("EnglishWords.dat");
-    string word;
-
-    word = getLine("Enter starting word: ");
-    cout << "The embedded words are:" << endl;
-    embeddedWords("", word, lexicon);
+    Vector<int> weights;
+    weights += 1, 3, 7, 13;
+    for (int i = 0; i < 20; i++) {
+        printMeasurable(i, weights);
+    }
     return 0;
 }
 
-
-void embeddedWords(string sofar, string letters, const Lexicon & lexicon) {
-    if (letters.size() == 0 &&
-            lexicon.contains(sofar)) {
-        cout << sofar << endl;
-    }
-    if (letters.size() > 0) {
-        string rest = letters.substr(1);
-        embeddedWords(sofar, rest, lexicon);
-        embeddedWords(sofar + letters[0], rest, lexicon);
+bool isMeasurable(int target, const Vector<int> & weights, Vector<int> way) {
+    if (weights.size() == 1) {
+        bool measured = target == weights[0];
+        if (measured) {
+            way.add(weights[0]);		// just for printing
+            cout << way.toString() << endl;
+        }
+        return measured;
+    } else {
+        Vector<int> rest = weights.subList(1, weights.size() - 1);
+        Vector<int> asWeight = way;
+        Vector<int> asCounterWeight = way;
+        asWeight.add(weights[0]);
+        asCounterWeight.add(-1 * weights[0]);
+        return isMeasurable(target, rest, way) ||	// excluding the weight
+                isMeasurable(target - weights[0], rest, asWeight) ||	// using the weight on the "weights" side
+                isMeasurable(target + weights[0], rest, asCounterWeight);	// using the weight with the "mass" side
     }
 }
