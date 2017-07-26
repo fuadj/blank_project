2e0c7debba594870fdb7001d3e0aa7ff7421cb4f
Fuad Seffa <fuadjemal@gmail.com>
2017-07-26 13:06:28 +0300

    Book string permutations solution

--- src/app.cpp
+++ src/app.cpp
@@ -1,59 +1,34 @@
 #include <iostream>
 #include <string>
 #include "console.h"
-#include "vector.h"
+#include "set.h"
 #include "simpio.h"
-#include "card.h"
 
 using namespace std;
 
-int countFifteens(Vector<Card> & cards);
+Set<string> generatePermutations(string s);
 
 int main() {
-    Vector<Card> cards;
-    cards += Card("5C"), Card("5D"), Card("5H"), Card("5S"), Card("JC");
-    cout << "Num fifteen " << countFifteens(cards) << endl;
+    string str = getLine("Enter a string: ");
+    cout << "The permutations of \"" << str << "\" are:" << endl;
+    for (string s : generatePermutations(str)) {
+        cout << "  \"" << s << "\"" << endl;
+    }
     return 0;
 }
 
-int getCardValue(Card & card) {
-    int rank = card.getRank();
-    if (rank == ACE)
-        return 1;
-    else if (rank >= JACK)
-        return 10;
-    else
-        return rank;
-}
-
-int countFifteensRecursive(Vector<Card> chosenCards, Vector<Card> & cards) {
-    if (cards.isEmpty()) {
-        int sum = 0;
-        for (Card card : chosenCards) {
-            sum += getCardValue(card);
-        }
-
-        if (sum == 15) {
-            for (int i = 0; i < chosenCards.size(); i++) {
-                if (i != 0) cout << " + ";
-                cout << chosenCards[i].toString();
-            }
-            cout << endl;
-        }
-        return sum == 15 ? 1 : 0;
+Set<string> generatePermutations(string s) {
+    Set<string> result;
+    if (s == "") {
+        result += "";
     } else {
-        Vector<Card> rest;
-        if (cards.size() > 1)	// calling subList(1, n) on Vector of size 1 causes Exception
-            rest = cards.subList(1, cards.size() - 1);
-
-        int count = countFifteensRecursive(chosenCards, rest);		// exclude the card
-        chosenCards.add(cards[0]);
-        count += countFifteensRecursive(chosenCards, rest);		// include it
-        return count;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[0];
+            string rest = s.substr(0, i) + s.substr(i + 1);
+            for (string sub : generatePermutations(rest)) {
+                result += ch + sub;
+            }
+        }
     }
-}
-
-int countFifteens(Vector<Card> & cards) {
-    Vector<Card> chosenCards;
-    return countFifteensRecursive(chosenCards, cards);
+    return result;
 }
